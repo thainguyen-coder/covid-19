0e7431af096c812dbeaa694e1b730094de994fc1
Nguyên Trần <tranthainguyen10git config --global user.email tranthainguyen10@gmail.com>
2021-06-02 09:17:44 +0700

    create selector countries

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
-
+import { sortBy } from 'lodash';
+import { useEffect, useState } from 'react';
+import { getCountries } from './apis';
 import CountrySelector from './components/CountrySelector';
 import HighLight from './components/HighLight';
 import Sumary from './components/Sumary';
 
 function App() {
+   const [countries, setCountries] =useState([]);
+  useEffect(() =>{
+    getCountries().then((res) =>{
+        console.log({res});
+       const countries = sortBy(res.data,'Country')
+        setCountries(countries);
+    });
+},[]);
   return (
     <>
-      <CountrySelector></CountrySelector>
+      <CountrySelector countries ={countries} ></CountrySelector>
       <HighLight></HighLight>
       <Sumary></Sumary>
     </>
--- src/components/CountrySelector/index.js
+++ src/components/CountrySelector/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import { FormControl, InputLabel, NativeSelect } from "@material-ui/core";
+import React, { useEffect } from "react";
+import { getCountries } from "../../apis";
 
-function index(props) {
-    return (
-        <div>
-            <h1>Đây là CountrySelector</h1>
-        </div>
-    );
+function index({value, handleOnChange, countries}) {
+
+
+  return (
+    <FormControl>
+      <InputLabel htmlFor="country-selector" shrink> Quốc gia</InputLabel>
+      <NativeSelect 
+      value={value}
+      onChange={handleOnChange}
+      inputProps={{
+          name: 'country',
+          id: 'country-selector'
+      }}>
+          {
+          countries.map((country) =>{
+            return <option value={country.ISO2.toLowerCase()}> {country.Country}</option>
+              
+          })
+      }
+      </NativeSelect>
+    </FormControl>
+  );
 }
 
 export default index;
